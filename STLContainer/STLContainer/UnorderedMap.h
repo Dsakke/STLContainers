0610b0269626a0682f1ac549529f8090847ba663
sam vanden bosch <sammeke119@live.com>
2023-09-02 15:56:31 +0200

    Added modifier functions

--- STLContainer/STLContainer/UnorderedMap.h
+++ STLContainer/STLContainer/UnorderedMap.h
@@ -2,13 +2,15 @@
 #include <type_traits>
 #include <memory>
 #include <cassert>
+#include <functional>
+
 namespace Containers
 {
 	// I should prob move this to another file
-	template<class t1, class t2>
+	template<class _T1, class _T2>
 	struct Pair
 	{
-		Pair(t1 item1, t2 item2)
+		Pair(_T1 item1, _T2 item2)
 			: Item1{item1}
 			, Item2{item2}
 		{}
@@ -24,14 +26,16 @@ namespace Containers
 			Item2 = other.Item2;
 		}
 
-		t1 Item1;
-		t2 Item2;
+		_T1 Item1;
+		_T2 Item2;
 	};
 
-	template<class key, class t, class hash, class allocator = std::allocator<Pair<key, t>>>
+	template<class _Key, class _T, class _Hash, class _Allocator = std::allocator<Pair<_Key, _T>>>
 	class UnorderedMap final
 	{
-	private:
+		static_assert(std::is_integral < decltype(std::function{ _Hash::Operator })::result_type > ::value);
+
+	public:
 		struct Bucket
 		{
 			Bucket() = default;
@@ -40,11 +44,11 @@ namespace Containers
 			Bucket& operator=(const Bucket& rhs);
 			Bucket& operator=(Bucket&& rhs);
 
-			Pair<key, t> pair;
+			Pair<_Key, _T> pair;
 			bool isAssigned = false;
-			Bucket pNext; // We expect the hash function to have collisions so we store everything as a basic linked list
+			Bucket* pNext; // We expect the hash function to have collisions so we store everything as a basic linked list
 		};
-	public:
+
 #pragma region RuleOf5
 		UnorderedMap(const UnorderedMap& other);
 		UnorderedMap(UnorderedMap&& other);
@@ -55,7 +59,7 @@ namespace Containers
 #pragma region Constructors
 
 		UnorderedMap();
-		UnorderedMap(uint32_t bucketCount, const hash& hash, const allocator& alloc);
+		UnorderedMap(uint32_t bucketCount, const _Hash& hash, const _Allocator& alloc);
 		
 #pragma endregion
 #pragma region Capacity
@@ -65,20 +69,20 @@ namespace Containers
 #pragma region Modifiers
 		void Clear();
 		// TODO add insert/insert_or_assign/emplace.e,placehint/try_emplace/erase/extract (everything that requires iterators)
-		uint32_t Erase(const key& key);
+		uint32_t Erase(const _Key& key);
 		void Swap(UnorderedMap& other);
-		Bucket* Extract(const key& key);
+		Bucket* Extract(const _Key& key);
 		// TODO add merge
 #pragma endregion
 #pragma region Lookup
-		_NODISCARD t& At(const key& key);
-		_NODISCARD const t& At(const key& key) const;
-		_NODISCARD t& operator[](const key& key);
-		_NODISCARD const t& operator[](const key& key) const;
-		_NODISCARD uint32_t Count(const key& key) const;
+		_NODISCARD _T& At(const _Key& key);
+		_NODISCARD const _T& At(const _Key& key) const;
+		_NODISCARD _T& operator[](const _Key& key);
+		_NODISCARD const _T& operator[](const _Key& key) const;
+		_NODISCARD uint32_t Count(const _Key& key) const;
 		_NODISCARD uint32_t Count(const Key& key, uint32_t hash) const;
-		_NODISCARD bool Contains(const key& key) const;
-		_NODISCARD bool Contains(const key& key, uint32_t hash) const;
+		_NODISCARD bool Contains(const _Key& key) const;
+		_NODISCARD bool Contains(const _Key& key, uint32_t hash) const;
 		// TODO: add equal_range
 #pragma endregion
 #pragma region Hash Policy
@@ -89,7 +93,7 @@ namespace Containers
 		void Reserve(uint32_t count);
 #pragma endregion
 	private:
-		allocator m_Allocator;
+		_Allocator m_Allocator;
 		Bucket* m_pBuckets;
 		uint32_t m_NrBuckets;
 		float m_MaxLoadFactor = 2.f;
@@ -100,11 +104,12 @@ namespace Containers
 		Bucket* CopyBuckets(const UnorderedMap& other);
 		void DeleteBuckets();
 		void DeleteBucketRecursive(Bucket* pBucket);
+		Bucket* FindBucket(const _Key& val);
 #pragma endregion
 	};
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>::UnorderedMap(const UnorderedMap& other)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::UnorderedMap(const UnorderedMap& other)
 		: m_Allocator{ other.m_Allocator }
 		, m_pBuckets{nullptr}
 		, m_NrBuckets{other.m_NrBuckets}
@@ -113,8 +118,8 @@ namespace Containers
 		m_pBuckets = CopyBuckets(other);
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>::UnorderedMap(UnorderedMap&& other)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::UnorderedMap(UnorderedMap&& other)
 		: m_Allocator{std::move(other.m_Alocator)}
 		, m_pBuckets { other.m_pBuckets }
 		, m_NrBuckets{ other.m_NrBuckets }
@@ -124,8 +129,8 @@ namespace Containers
 		other.m_NrBuckets = 0;
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>& UnorderedMap<key, t, hash, allocator>::operator=(const UnorderedMap& other)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>& UnorderedMap<_Key, _T, _Hash, _Allocator>::operator=(const UnorderedMap& other)
 	{
 		if (m_pBuckets)
 		{
@@ -138,8 +143,8 @@ namespace Containers
 		m_MaxLoadFactor = other.m_MaxLoadFactor;
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>& UnorderedMap<key, t, hash, allocator>::operator=(UnorderedMap&& other)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>& UnorderedMap<_Key, _T, _Hash, _Allocator>::operator=(UnorderedMap&& other)
 	{
 		if (m_pBuckets)
 		{
@@ -156,15 +161,15 @@ namespace Containers
 		// No need to set the other values to null, as they won't cause problems
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>::~UnorderedMap()
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::~UnorderedMap()
 	{
 		DeleteBuckets();
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>::UnorderedMap()
-		: m_Allocator{ allocator{} }
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::UnorderedMap()
+		: m_Allocator{ _Allocator{} }
 		, m_pBuckets{nullptr}
 		, m_NrBuckets{DEFAULT_NR_BUCKETS}
 		, m_MaxLoadFactor{2.f}
@@ -172,8 +177,8 @@ namespace Containers
 		m_pBuckets = m_Allocator.allocate(m_NrBuckets);
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>::UnorderedMap(uint32_t bucketCount, const hash& hash, const allocator& alloc)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::UnorderedMap(uint32_t bucketCount, const _Hash& hash, const _Allocator& alloc)
 		: m_Allocator{alloc}
 		, m_NrBuckets{bucketCount}
 		, m_pBuckets{nullptr}
@@ -182,8 +187,8 @@ namespace Containers
 		m_pBuckets = m_Allocator.allocate(m_NrBuckets);
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline bool UnorderedMap<key, t, hash, allocator>::IsEmpty() const
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline bool UnorderedMap<_Key, _T, _Hash, _Allocator>::IsEmpty() const
 	{
 		for (uint32_t i{}; i < m_NrBuckets; ++i)
 		{
@@ -195,8 +200,8 @@ namespace Containers
 		return true;
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline uint32_t UnorderedMap<key, t, hash, allocator>::GetSize() const
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline uint32_t UnorderedMap<_Key, _T, _Hash, _Allocator>::GetSize() const
 	{
 		uint32_t totalCount{};
 		for (uint32_t i{}; i < m_NrBuckets; ++i)
@@ -221,8 +226,108 @@ namespace Containers
 		return totalCount;
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline UnorderedMap<key, t, hash, allocator>::Bucket* CopyBuckets(const UnorderedMap<key, t, hash, allocator>& other)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline void UnorderedMap<_Key, _T, _Hash, _Allocator>::Clear()
+	{
+		for (int i = 0; i < m_NrBuckets; ++i)
+		{
+			if (m_pBuckets[i].pNext)
+			{
+				m_pBuckets[i].isAssigned = false;
+
+				// could also set buckets within the same bucket to isAssigned = false
+				// This is more memory intensive but should be faster
+				DeleteBucketRecursive(m_pBuckets[i].pNext);
+			}
+		}
+	}
+
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline uint32_t UnorderedMap<_Key, _T, _Hash, _Allocator>::Erase(const _Key& key)
+	{
+		auto hash = _Hash{}(val);
+
+		auto bucketIdx = hash % m_NrBuckets;
+
+		Bucket* pCurrentBucket = m_pBuckets[bucketIdx];
+		Bucket* pPrevBucket = nullptr;
+		while (pCurrentBucket)
+		{
+			if (pCurrentBucket->pair.Item1 == val)
+			{
+				break;
+			}
+
+			pPrevBucket = pCurrentBucket;
+			pCurrentBucket = pCurrentBucket->pNext;
+		}
+
+		if (pCurrentBucket)
+		{
+			if (pPrevBucket)
+			{
+				pPrevBucket->pNext = pCurrentBucket->pNext;
+				m_Allocator.deallocate(pCurrentBucket, 1);
+			}
+			else
+			{
+				if (pCurrentBucket->pNext)
+				{
+					*pCurrentBucket = *(pCurrentBucket->pNext)
+				}
+			}
+
+			return 1;
+		}
+		return 0;
+	}
+
+	template<class _Key, class t, class _Hash, class _Allocator>
+	inline void UnorderedMap<_Key, t, _Hash, _Allocator>::Swap(UnorderedMap& other)
+	{
+		_Allocator tempAlloc = other.m_Allocator;
+		Bucket* pTempBuckets = other.m_pBuckets;
+		uint32_t tempNrBuckets = other.m_NrBuckets;
+		float tempMaxLoadFactor = other.m_MaxLoadFactor;
+
+		other.m_Allocator = m_Allocator;
+		other.m_pBuckets = m_pBuckets;
+		other.m_NrBuckets = m_NrBuckets;
+		other.m_MaxLoadFactor = m_MaxLoadFactor;
+
+		m_Allocator = tempAlloc;
+		m_pBuckets = pTempBuckets;
+		m_NrBuckets = tempNrBuckets;
+		m_MaxLoadFactor = tempMaxLoadFactor;
+	}
+
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::Bucket* UnorderedMap<_Key, _T, _Hash, _Allocator>::Extract(const _Key& key)
+	{
+		auto hash = _Hash{}(val);
+
+		auto bucketIdx = hash % m_NrBuckets;
+
+		Bucket* pCurrentBucket = m_pBuckets[bucketIdx];
+		Bucket* pPrevBucket = nullptr;
+		while (pCurrentBucket)
+		{
+			if (pCurrentBucket->pair.Item1 == val)
+			{
+				break;
+			}
+
+			pPrevBucket = pCurrentBucket;
+			pCurrentBucket = pCurrentBucket->pNext;
+		}
+
+		pPrevBucket->pNext = pCurrentBucket->pNext;
+		pCurrentBucket->pNext = nullptr;
+		return pCurrentBucket;
+	}
+
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::Bucket* CopyBuckets(const UnorderedMap<_Key, _T, _Hash, _Allocator>& other)
 	{
 		Bucket* pOut = m_Allocator.allocate(other.m_NrBuckets);
 		for (uint32_t i{}; i < other.m_NrBuckets; ++i)
@@ -245,8 +350,8 @@ namespace Containers
 		return pOut;
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline void UnorderedMap<key, t, hash, allocator>::DeleteBuckets()
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline void UnorderedMap<_Key, _T, _Hash, _Allocator>::DeleteBuckets()
 	{
 		for (uint32_t i{}; i < m_NrBuckets; ++i)
 		{
@@ -258,13 +363,35 @@ namespace Containers
 		m_Allocator.deallocate(m_pBuckets, m_NrBuckets);
 	}
 
-	template<class key, class t, class hash, class allocator>
-	inline void UnorderedMap<key, t, hash, allocator>::DeleteBucketRecursive(Bucket* pBucket)
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline void UnorderedMap<_Key, _T, _Hash, _Allocator>::DeleteBucketRecursive(Bucket* pBucket)
 	{
 		if (pBucket->pNext)
 		{
 			DeleteBucketRecursive(pBucket.pNext);
 		}
 		m_Allocator.deallocate(pBucket, 1);
+	}
+
+	template<class _Key, class _T, class _Hash, class _Allocator>
+	inline UnorderedMap<_Key, _T, _Hash, _Allocator>::Bucket* UnorderedMap<_Key, _T, _Hash, _Allocator>::FindBucket(const _Key& val)
+	{
+		static_assert(std::is_integral < decltype(std::function{ _Hash::Operator })::result_type >::value )
+		auto hash = _Hash{}(val);
+
+		auto bucketIdx = hash % m_NrBuckets;
+
+		Bucket* pCurrentBucket = m_pBuckets[bucketIdx];
+		while (pCurrentBucket)
+		{
+			if (pCurrentBucket->pair.Item1 == val)
+			{
+				return pCurrentBucket;
+			}
+
+			pCurrentBucket = pCurrentBucket->pNext;
+		}
+
+		return nullptr;
 	}
 }
